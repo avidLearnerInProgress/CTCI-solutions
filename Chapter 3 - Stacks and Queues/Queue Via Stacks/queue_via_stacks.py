61befa57663bcb2d07f804f329b9ce5cdc897129
avidLearnerInProgress <chiragshah9696@gmail.com>
2019-06-23 21:43:27 +0530

    Added python solution

---
+++ Chapter 3 - Stacks and Queues/Queue Via Stacks/queue_via_stacks.py
@@ -0,0 +1,27 @@
+class Queue:
+    def __init__(self):
+        self.s1, self.s2 = [], []
+
+    def enqueue(self, data):
+        self.s1.append(data)
+    
+    def dequeue(self):
+        if self.s1 == [] and self.s2 == []:
+            return None
+        if self.s2 == []:
+            while not self.s1 == []:
+                self.s2.append(self.s1.pop())
+        res = self.s2.pop()
+        return res
+
+if __name__ == "__main__":
+    qs = Queue()
+    qs.enqueue(10)
+    qs.enqueue(20)
+    qs.enqueue(30)
+    print(qs.dequeue())
+    print(qs.dequeue())
+    print(qs.dequeue())
+
+
+        
